5d182270f509bfdc371f051fdbe5737f9d7a6164
eyoaladmasu2217 <eyoaladmasu@gmail.com>
2026-03-15 22:59:27 -0700

    Security: Add Strict-Transport-Security header

--- backend/config/security_headers.php
+++ backend/config/security_headers.php
@@ -4,5 +4,6 @@ header("X-Frame-Options: DENY");
 header("X-Content-Type-Options: nosniff");
 header("X-XSS-Protection: 1; mode=block");
 header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
 header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://chapa.link; video-src 'self' data:; connect-src 'self' https://api.chapa.co;");
 ?>
