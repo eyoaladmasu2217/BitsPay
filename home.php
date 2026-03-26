d267e33675b3a871adcd4000ed8c31cb98cc1159
eyoaladmasu2217 <eyoaladmasu@gmail.com>
2026-03-26 11:51:57 -0700

    UI: Refine status message alerts with better spacing and iconography

--- home.php
+++ home.php
@@ -54,10 +54,16 @@ if (!$wallet) {
 
         <div class="balance-card">
             <?php if (isset($_SESSION['success'])): ?>
-                <div class="status-msg status-success"><?php echo $_SESSION['success']; unset($_SESSION['success']); ?></div>
+                <div class="status-msg status-success">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>
+                    <?php echo $_SESSION['success']; unset($_SESSION['success']); ?>
+                </div>
             <?php endif; ?>
             <?php if (isset($_SESSION['error'])): ?>
-                <div class="status-msg status-error"><?php echo $_SESSION['error']; unset($_SESSION['error']); ?></div>
+                <div class="status-msg status-error">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" /></svg>
+                    <?php echo $_SESSION['error']; unset($_SESSION['error']); ?>
+                </div>
             <?php endif; ?>
 
             <p class="balance-label">Total Balance</p>
